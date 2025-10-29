8fa19442c79d201c8022893207ae56a6dbeb2d7d
BecksChomoev <chomoevbeksultan@gmail.com>
2025-10-29 15:29:05 -0400

    deleted String import.

--- Week 3/string1.py
+++ Week 3/string1.py
@@ -2,8 +2,6 @@
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
-from lib2to3.fixer_util import String
-
 
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
