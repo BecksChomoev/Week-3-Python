581d28f13175844ceae2668db04afcd9a59dd74d
BecksChomoev <chomoevbeksultan@gmail.com>
2025-10-31 14:54:40 -0400

    Advanced tip calculator is done.

--- Week 4/Tip_Calculator.py
+++ Week 4/Tip_Calculator.py
@@ -1,10 +1,18 @@
-print("Welcome to Calculator")
-number_of_guests = int(input("How many guests do you have? "))
+print("Welcome to Tip Calculator")
+number_of_guests = int(input("How many people are splitting the bill?"))
+guests = []
+while len(guests) < number_of_guests:
+    guests.append(input("What is the guest name? Please enter each guest separately."))
+
 receipt = float(input("How much was your total receipt?"))
 tip = int(input("How much percent did you tip?"))
+if tip >= 20:
+    print('Wow! Thank you for your generosity!')
 tip_amount = (tip / 100) * receipt
 total = tip_amount + receipt
 
 print("The tip amount is $" + str(tip_amount))
 print("Your total receipt is $" + str(total))
-print("Each one of you should pay $" + str(total / number_of_guests))
+
+for guest in guests:
+    print("%s: should pay $" % guest + str(total / number_of_guests))
