4c7d139c4d3b3afcfd1ebe7afa604740e15454df
BecksChomoev <chomoevbeksultan@gmail.com>
2025-11-05 10:15:23 -0500

    Imported random function and wrote a welcoming message.

---
+++ Week 5/Number Guessing Game.py
@@ -0,0 +1,7 @@
+from random import randrange
+
+random_number = randrange(1, 100)
+user_attempts = 0
+
+print("Welcome to the Number Guessing Game!")
+print("You have to guess the number between 1 and 100. You have 5 attempts to guess the number.")
