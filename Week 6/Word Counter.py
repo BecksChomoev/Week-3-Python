06589b85f44415908dae194daf1a70ba96e88025
BecksChomoev <chomoevbeksultan@gmail.com>
2025-11-06 17:40:45 -0500

    Added improvement to remove the additional fact.

--- Week 6/Word Counter.py
+++ Week 6/Word Counter.py
@@ -1,5 +1,8 @@
+import string
+
+translator = str.maketrans('', '', string.punctuation)
 user_input = input('Welcome to Word Counter program! '
-                   'The program will count the occurrences of each word. Enter your sentence: ').lower()
+                   'The program will count the occurrences of each word. Enter your sentence: ').lower().translate(translator)
 occurrences ={}
 
 user_input = user_input.split()
