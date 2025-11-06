d61538fdb5a50e382345f95ed689f3802cd08c53
BecksChomoev <chomoevbeksultan@gmail.com>
2025-11-06 17:14:04 -0500

    Word Counter is done.

---
+++ Week 6/Word Counter.py
@@ -0,0 +1,13 @@
+user_input = input('Welcome to Word Counter program! '
+                   'The program will count the occurrences of each word. Enter your sentence: ').lower()
+occurrences ={}
+
+user_input = user_input.split()
+
+for word in user_input:
+    if word in occurrences:
+        occurrences[word] += 1
+    else:
+        occurrences[word] = 1
+
+print(occurrences)
